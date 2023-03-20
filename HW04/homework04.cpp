18b33ecefb0da3a8683a4728641281a57557c109
bulat <bulatxisameev@gmail.com>
2023-03-20 21:30:30 +0300

    Сделал запуск через консоль (./Work4 --rules /rules.txt --directory /myfiles)(были проблемы с ../ в начале пути). К сожалению, не смог обработать файл правил и внедрить в программу(в частности не получилось достать построчно текст из документа). Добавил обработку для одинаковых файлов(добавляю _copy в название).     При повторном запуске программа работает корректно, ничего не изменяет.

--- HW04/homework04.cpp
+++ HW04/homework04.cpp
@@ -11,12 +11,38 @@ namespace po = boost::program_options;
 namespace fs = boost::filesystem;
 
 
+void show_Directory(const fs::path in_folder) { //изображение папки
+    if (fs::exists(in_folder)) {
+        if (fs::is_regular_file(in_folder)) {
+            std::cout << "Regular file: " << in_folder << std::endl;
+        } else if (fs::is_directory(in_folder)) {
+            for (const auto &entry: fs::directory_iterator(in_folder)) {
+                fs::file_status st = entry.status();
+                switch (st.type()) {
+                    case fs::regular_file:
+                        std::cout << "--" << entry.path() << " (File) \n" << std::endl;
+                        break;
+                    case fs::directory_file:
+                        std::cout << "-----------------" << entry.path() << " (Directory) " << std::endl;
+                        show_Directory(entry);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    } else {
+        std::cout << in_folder << " does not exist!\n";
+    }
+}
+
 void directories(boost::filesystem::path src) { //создание папок, если они отсутвуют для фалов
     try {
-        fs::path a = src;
-        fs::path b = src;
-        fs::path c = src;
-        fs::path d = src;
+        fs::path a = src.string();
+        fs::path b = src.string();
+        fs::path c = src.string();
+        fs::path d = src.string();
         fs::path img(a.concat("/Image"));
         fs::path doc(b.concat("/Docs"));
         fs::path mus(c.concat("/Music"));
@@ -47,60 +73,83 @@ void delete_Directory(boost::filesystem::path src) { //удаление пуст
 }
 
 void redirect(boost::filesystem::path src) { //распределение папок по их расширению на папки
-    if (src.extension() == ".jpg" ||
-        src.extension() == ".png") {
-        fs::path a = ("../myfiles/Image/");
-        fs::copy(src, a.concat(src.filename().string()));
-        fs::remove(src);
-    }
-    if (src.extension() == ".txt" ||
-        src.extension() == ".docx") {
-        fs::path b = ("../myfiles/Docs/");
-        fs::copy(src, b.concat(src.filename().string()));
-        fs::remove(src);
-    }
-    if (src.extension() == ".mp3") {
-        fs::path c = ("../myfiles/Music/");
-        fs::copy(src, c.concat(src.filename().string()));
-        fs::remove(src);
-    }
-    if (src.extension() != ".mp3" &&
-        src.extension() != ".txt" &&
-        src.extension() != ".docx" &&
-        src.extension() != ".jpg" &&
-        src.extension() != ".png") {
-        fs::path d = ("../myfiles/Others/");
-        fs::copy(src, d.concat(src.filename().string()));
-        fs::remove(src);
-    }
-}
-
-void show_Directory(const fs::path &in_folder) { //изображение папки
-    if (fs::exists(in_folder)) {
-        if (fs::is_regular_file(in_folder)) {
-            std::cout << "Regular file: " << in_folder << std::endl;
-        } else if (fs::is_directory(in_folder)) {
-            for (const auto &entry: fs::directory_iterator(in_folder)) {
-                fs::file_status st = entry.status();
-                switch (st.type()) {
-                    case fs::regular_file:
-                        std::cout << "--" << entry.path() << " (File) \n" << std::endl;
-                        break;
-
-                    case fs::directory_file:
-                        std::cout << "-----------------" << entry.path() << " (Directory) " << std::endl;
-                        show_Directory(entry);
-                        break;
-
-                    default:
-                        break;
+    try {
+        if (src.extension() == ".jpg" ||
+            src.extension() == ".png") {
+            fs::path a = ("./myfiles/Image/");
+            fs::path aa = ("./myfiles/Image/");
+            if (exists(aa.concat(src.filename().string()))) {
+                if (src.parent_path() != "./myfiles/Image") {
+                    std::cout << src.parent_path();
+                    a.concat(src.stem().string());
+                    a.concat("_copy");
+                    a.concat(src.extension().string());
+                    fs::copy(src, a);
+                    fs::remove(src);
                 }
+            } else {
+                fs::copy(src, aa);
+                fs::remove(src);
             }
         }
-    } else {
-        std::cout << in_folder << " does not exist!\n";
+        if (src.extension() == ".txt" ||
+            src.extension() == ".docx") {
+            fs::path b = ("./myfiles/Docs/");
+            fs::path bb = ("./myfiles/Docs/");
+            if (exists(bb.concat(src.filename().string()))) {
+                if (src.parent_path() != "./myfiles/Docs") {
+                    b.concat(src.stem().string());
+                    b.concat("_copy");
+                    b.concat(src.extension().string());
+                    fs::copy(src, b);
+                    fs::remove(src);
+                }
+            } else {
+                fs::copy(src, bb);
+                fs::remove(src);
+            }
+        }
+        if (src.extension() == ".mp3") {
+            fs::path c = ("./myfiles/Music/");
+            fs::path cc = ("./myfiles/Music/");
+            if (exists(cc.concat(src.filename().string()))) {
+                if (src.parent_path() != "./myfiles/Music") {
+                    c.concat(src.stem().string());
+                    c.concat("_copy");
+                    c.concat(src.extension().string());
+                    fs::copy(src, c);
+                    fs::remove(src);
+                }
+            } else {
+                fs::copy(src, cc);
+                fs::remove(src);
+            }
+        }
+        if (src.extension() != ".mp3" &&
+            src.extension() != ".txt" &&
+            src.extension() != ".docx" &&
+            src.extension() != ".jpg" &&
+            src.extension() != ".png") {
+            fs::path d = ("./myfiles/Others/");
+            fs::path dd = ("./myfiles/Others/");
+            if (exists(dd.concat(src.filename().string()))) {
+                if (src.parent_path() != "./myfiles/Others") {
+                    d.concat(src.stem().string());
+                    d.concat("_copy");
+                    d.concat(src.extension().string());
+                    fs::copy(src, d);
+                    fs::remove(src);
+                }
+            } else {
+                fs::copy(src, dd);
+                fs::remove(src);
+            }
+        }
+    } catch (const fs::filesystem_error &e) {
+        std::cerr << e.what();
     }
 }
+
 
 void start_Work(boost::filesystem::path src) {
     try {
@@ -129,40 +178,30 @@ int main(int argc, char **argv) {
     po::options_description desc("Allowed options");
     desc.add_options()
             ("help,h", "print usage message")
-            ("input,i", po::value<std::string>()->default_value("text"), "Input text")
-            ("output,o", po::value<std::string>()->default_value("out.txt"), "Output file")
-            ("rules,r", po::value<std::string>()->default_value(":\n"           //выводим правила нашей программы
-                                                                "jpg:Images\n"
-                                                                "png:Images\n"
-                                                                "mp3:Music\n"
-                                                                "txt:Docs\n"
-                                                                "docx:Docs\n"), " Rules:\n"
-                                                                                "jpg:Images\n"
-                                                                                "png:Images\n"
-                                                                                "mp3:Music\n"
-                                                                                "txt:Docs\n"
-                                                                                "docx:Docs\n");
+            ("rules,r", po::value<std::string>()->default_value("/rules"), "Rules /rules")
+            ("directory,r", po::value<std::string>()->default_value("../myfiles"), "Directory ../myfiles");
 
     // Parse command line arguments
     po::variables_map vm;
     po::store(po::command_line_parser(argc, argv).options(desc).run(), vm);
     po::notify(vm);
 
-    if (vm.count("help") || !vm.count("input") || !vm.count("output") || !vm.count("rules")) {
+    if (vm.count("help") || !vm.count("rules") || !vm.count("directory")) {
         std::cerr << desc << std::endl;
         return 1;
     }
 
-    std::string input = vm["input"].as<std::string>();
-    std::string output = vm["output"].as<std::string>();
-    std::string rules = vm["rules"].as<std::string>();
+    std::string input = vm["rules"].as<std::string>();
+    std::string filename = vm["directory"].as<std::string>();
 
-    std::cout << "Input " << input << std::endl;
-    std::cout << "Output " << output << std::endl;
-    std::cout << "Rules " << rules << std::endl;
+    fs::path rules = input;
+    fs::path direc = filename;
 
-    show_Directory("../myfiles");
-    directories("../myfiles");
-    start_Work("../myfiles");
-    show_Directory("../myfiles");
+    std::cout << "Rules: " << rules << std::endl;
+    std::cout << "File to refactor: " << direc << std::endl;
+
+    fs::path a = ".";
+    show_Directory(a.concat(direc.string()));
+    directories(a);
+    start_Work(a);
 }
