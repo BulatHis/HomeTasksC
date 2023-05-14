548fa9c2f070b8b77a718d10734b75548f2101ee
bulat <bulatxisameev@gmail.com>
2023-05-14 13:54:28 +0300

    исправил работу, ошибка была в том, что не обрабатывался символ "~"

--- HWServer/Server.cpp
+++ HWServer/Server.cpp
@@ -36,10 +36,10 @@ public:
             std::string line;
             std::getline(is, line);// считываем строку из потока в переменную line
             std::cout << line << std::endl;
-                if (line =="username\r"||line == "hostname\r" || line == "serverdatetime\r" || line.substr(0, 7) == "mkfile "
-                ||line.substr(0, 6) == "mkdir " || line.substr(0, 3) == "ls " || line == "quit\r"
-                ||line.substr(0, 6) == "rmdir " || line.substr(0, 7) == "rmfile "
-                || line.substr(0, 9) == "readfile "){
+                if (line =="username\r"||line == "hostname\r" || line == "serverdatetime\r" || line.substr(0, 9) == "mkfile ~/"
+                ||line.substr(0, 8) == "mkdir ~/" || line.substr(0, 5) == "ls ~/" || line == "quit\r"
+                ||line.substr(0, 8) == "rmdir ~/" || line.substr(0, 9) == "rmfile ~/"
+                || line.substr(0, 11) == "readfile ~/"){
                 handle_command(con_handle,line.substr(0,line.length()-1)) ;
                 //проверка на команды внутри сообщений
             }
@@ -132,36 +132,39 @@ public:
         } else if (cmd == "serverdatetime") { // обработка команды запроса времени
             response = "Server datetime: " + get_server_datetime() + "\n";
             std::cout << response << std::endl;
-        } else if (cmd.substr(0, 3) == "ls ") { //проверяем первые 3 символа строки, если они == ls , то обрабатываем
+        } else if (cmd.substr(0, 5) == "ls ~/") { //проверяем первые 3 символа строки, если они == ls , то обрабатываем
             boost::process::ipstream out;//создание потока для чтения вывода команды
-            boost::process::system(cmd, boost::process::std_out > out);// выполнение команды и запись ответа в out
+            std::string command = "ls " + cmd.substr(5);
+            boost::process::system(command, boost::process::std_out > out);// выполнение команды и запись ответа в out
             std::stringstream response_stream; //создание потока для записи вывода в переменную response_stream
             response_stream << out.rdbuf();// запись вывода команды в переменную response_stream
             response = response_stream.str(); //вывод в терминал
-        } else if (cmd.substr(0, 7) == "mkfile ") {
-            std::string path = cmd.substr(7);// запишем в переменную имя файла для красивого вывода
-            boost::process::system(cmd);// ну и просто выполняем команду mkfile + path
-            response = "File created: " + path + "\n";
-        } else if (cmd.substr(0, 6) == "mkdir ") {
-            std::string path = cmd.substr(6);
-            boost::process::system(cmd); // опять же команда в теринал
+        }else if (cmd.substr(0, 9) == "mkfile ~/") {
+                std::string path = cmd.substr(9);
+                std::string command ="touch " + path;
+                boost::process::system(command);
+                response = "File created: " + path + "\n";
+            } else if (cmd.substr(0, 8) == "mkdir ~/") {
+            std::string path = cmd.substr(8);
+            std::string command ="mkdir " + path;
+            boost::process::system(command); // опять же команда в теринал
              //решил попробовать так, а не через  fs::create_directory(path);
             response = "Directory created: " + path + "\n";
-        }else if (cmd.substr(0, 7) == "rmfile ") {
-                std::string path = cmd.substr(7); //запишем в переменную имя файла
+        }else if (cmd.substr(0, 9) == "rmfile ~/") {
+                std::string path = cmd.substr(9); //запишем в переменную имя файла
                 std::string command = "rm " + path;// команда, которую будем вводить в терминал
                 boost::process::system(command);
                 response = "File removed: " + path + "\n";
-        } else if (cmd.substr(0, 6) == "rmdir ") { // аналогично
-                std::string path = cmd.substr(6);
+        } else if (cmd.substr(0, 8) == "rmdir ~/") { // аналогично
+                std::string path = cmd.substr(8);
                 std::string command = "rm -rf " + path; // немного отличается команда
                 boost::process::system(command);
                 response = "Directory removed: " + path + "\n";
-        } else if (cmd.substr(0, 9) == "readfile ") {
+        } else if (cmd.substr(0, 11) == "readfile ~/") {
             std::vector<std::string> tokens;//создадим вектор, что бы удобно разделить команду на 3 части (readfile, имя файла,размер чтения*если есть*)
             boost::algorithm::split(tokens,cmd,boost::is_any_of(" "));// такой вот метод сплита :)
             if (tokens.size() >= 2) { //проверка на наличие имени файла
-                std::string path = tokens[1]; //выносим в переменную имя файла
+                std::string path = tokens[1].substr(2); //выносим в переменную имя файла
                 int num_bytes = -1; // переменная для размера чтения (-1, чтобы начать с 0)
                 if (tokens.size() == 3) {//проверка на присутсвия параметра размера чтения
                     num_bytes = std::stoi(tokens[2]); //запись размера в переменную
